203e6ffde5c461d2c79dd9b725f66b419364f814
sindhu-horatti <84697125+sindhu-horatti@users.noreply.github.com>
2021-06-24 14:39:50 +0530

    Done

--- Program-1.c
+++ Program-1.c
@@ -1,29 +1,18 @@
-//Program to find number of digits in the input
-//INPUT - 452
-//OUTPUT -3
-
 #include <stdio.h>
 
 int main()
 {
    int num;
     int count = 0;
-
-    /* Input number from user */
-    printf("Enter any number: ");
+    //printf("Enter any number: ");
     scanf("%d", &num);
-
-    /* Run loop till num is greater than 0 */
     do
     {
-        /* Increment digit count */
         count++;
-
-        /* Remove last digit of 'num' */
         num /= 10;
-    } while(  != 0);
+    } while(num!= 0);
 
-    printf("Total digits: %d", );
+    printf("%d",count );
 
     return 0;
 }
