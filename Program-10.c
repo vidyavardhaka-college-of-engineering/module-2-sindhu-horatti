646874f010dfcd48f18f643e9b70bd21311e10e9
sindhu-horatti <84697125+sindhu-horatti@users.noreply.github.com>
2021-06-24 15:10:28 +0530

    Done

--- Program-10.c
+++ Program-10.c
@@ -1,40 +1,31 @@
 //INPUT - 4 30 20 10 60
 //OUTPUT: The largest and smallest elements are 60 and 10
 
-//INSERT THE MISSING CODE 
 #include <stdio.h>
 
 int main()
 {
     int arr[100], n, i, small, large;
-    printf("Enter how many values you want to read and values");
     scanf("%d", &n);
 
     for (i = 0; i < n; i++)
     {
-        //printf("Enter the value of a[%d] : ",i);
         scanf("%d", &arr[i]);
     }
-
     small = arr[0];
     large = arr[0];
 
     for (i = 1; i < n; i++)
     {
-        if (arr[i] < small )//missing code
+        if (arr[i] < small)
         {
-            small = arr[i];//missing code
+            small = arr[i];
         }
-        
-        if (arr[i] >large  )//missing code
+        if (arr[i] >large)
         {
-            large= arr[];//missing code
+            large= arr[i];
         }
     }
-
-    printf("The largest element of the array = %d", large);
-    printf("The smallest element of the array = %d",);
-
+    printf(" The largest and smallest elements are %d and %d ", large,small);
     return 0;
 }
-
