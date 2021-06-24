c947c0d6183a44d3b01bbc7bbf0cc99a4620e62d
sindhu-horatti <84697125+sindhu-horatti@users.noreply.github.com>
2021-06-24 14:51:23 +0530

    Done

--- Program-3.c
+++ Program-3.c
@@ -7,11 +7,9 @@ int main() {
 	int i = 1, n;
 	printf("Enter an integer");
 	scanf("%d",&n);
- // Fill the code in scanf()
 	do {
 		printf("I Love VVCE ");
 		i++;
-		// complete the statement
-	} while(i<=);
-	return(0);// complete the condition
+	} while(i<=n);
+	return(0);
 }
